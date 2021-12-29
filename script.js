ab9b8544d750bb3c1a59b74e3bb7979acc2b4dee
balalis43 <balalis43@gmail.com>
2021-12-29 22:48:24 +0000

    Miserable js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 (function () {
     const appRootElement = document.getElementById('app-root');
     appRootElement.innerText = 'Hello world!';
+    
+  const countWeekDaysBetween = (startDate, endDate) =>
+    Array
+        .from ({ length: (endDate =startDate) / (100 *3600*24) })
+        .reduce(count +> {
+            if (startDate.getDay(() % 6 ! == 0) count++);
+            startDate = new Date(startDate.setDate(startDate.getDate()
+            return count;
+            ))
+        })
 })();
+
+
